6b610649dc963ca902d253f044d4b806244df48e
aldoGC9092 <aldo.garcia9092@alumnos.udg.mx>
2024-10-01 01:22:53 -0600

    first commit

---
+++ src/components/Block.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="block" v-show="showBlock" @click="stopTimer">
+    Dame click!
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['delay'],
+  data() {
+    return {
+      showBlock: false,
+      timer: null,
+      reactionTime: 0
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true
+      this.startTimer()
+    }, this.delay)
+  },
+  methods: {
+    startTimer() {
+      // start the timer, tick every 10ms
+      this.timer = setInterval(() => {
+        this.reactionTime += 10
+      }, 10)
+    },
+    stopTimer() {
+      // stop the timer
+      clearInterval(this.timer)
+      this.$emit('end', this.reactionTime)
+    }
+  }
+}
+</script>
+
+<style>
+  .block {
+    width: 200px;
+    border-radius: 10px;
+    background:  #184979;;
+    color: white;
+    text-align: center;
+    padding: 100px 0;
+    margin: 40px auto;
+  }
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,34 @@
+<template>
+  <p>Reaction time - {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ['score'],
+  data() {
+    return {
+      rank : null
+    }
+  },
+  mounted() {
+    if (this.score < 250) {
+      this.rank = 'Wow! Que rapidez'
+    } else if (this.score < 400) {
+      this.rank = 'No esta mal!'
+    } else if (this.score < 600) {
+      this.rank = 'Mucho que mejorar'
+    }else {
+      this.rank = 'Lento!'
+    }
+  }
+}
+</script>
+
+<style>
+  .rank {
+    font-size: 1.4em;
+    color: #62116e;
+    font-weight: bold;
+  }
+</style>
